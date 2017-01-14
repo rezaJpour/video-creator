03bc015584a852ca3d5ca0a810c21674539a907f
Joshua Laesch <laeschjs@rose-hulman.edu>
2017-01-13 20:39:26 -0500

    Added the video intent to show up when the icon is pressed

--- app/src/main/java/net/jackhallam/videocreator/pages/CameraFragment.java
+++ app/src/main/java/net/jackhallam/videocreator/pages/CameraFragment.java
@@ -1,25 +1,25 @@
 package net.jackhallam.videocreator.pages;
 
-import android.hardware.Camera;
-import android.media.MediaRecorder;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import net.jackhallam.videocreator.R;
 
-import java.io.IOException;
-import java.util.List;
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CameraFragment extends Fragment {
+
+    static final int REQUEST_VIDEO_CAPTURE  = 1;
 
     public CameraFragment() {
         // Required empty public constructor
@@ -29,6 +29,26 @@ public class CameraFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View inflatedView = inflater.inflate(R.layout.fragment_camera, container, false);
 
+        ImageView cameraImage = (ImageView) inflatedView.findViewById(R.id.cameraImage);
+        cameraImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+                    startActivityForResult(intent, REQUEST_VIDEO_CAPTURE );
+                }
+            }
+        });
+
         return inflatedView;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            Uri videoUri = intent.getData();
+            //TODO: do something with the Uri
+        }
+    }
+
 }
