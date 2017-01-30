29970eaca826fca993d7c31812596bf735570120
Joshua Laesch <laeschjs@rose-hulman.edu>
2017-01-30 18:42:48 -0500

    used AsyncTask to load videos off phone in bg

--- app/src/main/java/net/jackhallam/videocreator/VideoPickerAdapter.java
+++ app/src/main/java/net/jackhallam/videocreator/VideoPickerAdapter.java
@@ -78,44 +78,6 @@ public class VideoPickerAdapter extends RecyclerView.Adapter<VideoPickerAdapter.
         return deviceVideos.size();
     }
 
-//    @Override
-//    public View getView(int i, View contentView, ViewGroup parent) {
-//        View v = null;
-//        if(contentView == null){
-//            v = LayoutInflater.from(mContext).inflate(R.layout.video_view,parent,false);
-//        }else{
-//            v = contentView;
-//        }
-//        ImageView iv = (ImageView) v.findViewById(R.id.thumbnail);
-//        TextView tv = (TextView) v.findViewById(R.id.video_path);
-//        cursor.moveToPosition(cursor.getCount()-1-i);
-//        String filePath = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Thumbnails.DATA));
-//        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-//        //use one of overloaded setDataSource() functions to set your data source
-//        retriever.setDataSource(filePath);
-//        String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
-//        double timeInMillisec = Long.parseLong(time );
-//        timeInMillisec=timeInMillisec/1000.0;
-//        tv.setText(timeInMillisec+"");
-//        Bitmap bm = ThumbnailUtils.createVideoThumbnail(filePath, MediaStore.Video.Thumbnails.FULL_SCREEN_KIND);
-//        if(bm.getWidth()>bm.getHeight()){
-//            bm = Bitmap.createBitmap(bm, 0, 0, bm.getHeight(), bm.getHeight());
-//        } else {
-//            bm = Bitmap.createBitmap(bm, 0, 0, bm.getWidth(), bm.getWidth());
-//        }
-//        iv.setImageBitmap(bm);
-//        final Bitmap finalBm = bm;
-//        v.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                videoList.add(index, finalBm);
-//                adap.notifyDataSetChanged();
-//                ad.dismiss();
-//            }
-//        });
-//        return v;
-//    }
-
     public class ViewHolder  extends RecyclerView.ViewHolder{
 
         private ImageView image;
